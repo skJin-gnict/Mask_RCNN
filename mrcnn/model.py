341a194d41c55dd64fb33a7bcb9277eb0166da98
skJin-gnict <76027831+skJin-gnict@users.noreply.github.com>
2023-03-25 19:21:43 +0900

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2178,6 +2178,7 @@ class MaskRCNN(object):
             keras.regularizers.l2(self.config.WEIGHT_DECAY)(w) / tf.cast(tf.size(input=w), tf.float32)
             for w in self.keras_model.trainable_weights
             if 'gamma' not in w.name and 'beta' not in w.name]
+        print("trainable_weights:",self.keras_model.trainable_weights)
         print(reg_losses)
         self.keras_model.add_loss(tf.add_n(reg_losses))
 
